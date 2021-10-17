0308d1c8028ccc4362ea6d39e6f53c3adbcbcc47
Annee Zambrano <annee.zambrano.mt@correo.usa.edu.co>
2021-10-16 22:15:46 -0500

    proyecto springboot

---
+++ src/main/java/mintic_g25/proyectoclase/ProyectoclaseApplication.java
@@ -0,0 +1,13 @@
+package mintic_g25.proyectoclase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoclaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoclaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mintic_g25/proyectoclase/ProyectoclaseApplicationTests.java
@@ -0,0 +1,13 @@
+package mintic_g25.proyectoclase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoclaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
